f8088fdb4a6dbfd5362f357929708494522473bc
tomasmin <tomas.mineika@akka.eu>
2019-10-02 19:31:28 +0200

    delete words from custom vocab

--- app/src/main/java/com/tomas/vocabquiztest/ListAdapter.kt
+++ app/src/main/java/com/tomas/vocabquiztest/ListAdapter.kt
@@ -1,13 +1,20 @@
 package com.tomas.vocabquiztest
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldValue
 
-class ListAdapter(private val list: Map<String,String>)
+
+class ListAdapter(private val list: MutableMap<String,String>)
     : RecyclerView.Adapter<WordViewHolder>() {
+
+    private var removedPosition: Int = 0
+    private var removedKey: String = ""
+    private var removedValue: String = ""
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -18,6 +25,28 @@ class ListAdapter(private val list: Map<String,String>)
         val key = list.keys.toList()[position]
         val value = list.values.toList()[position]
         holder.bind(key, value)
+    }
+
+    fun removeItem(position: Int, viewHolder: RecyclerView.ViewHolder, docRef: DocumentReference) {
+        removedKey = list.keys.toList()[position]
+        removedValue = list[removedKey]!!
+        removedPosition = position
+
+        list.remove(removedKey)
+        val updates = hashMapOf<String, Any>(
+            removedKey to FieldValue.delete()
+        )
+
+        docRef.update(updates).addOnCompleteListener {
+            notifyItemRemoved(position)
+            Snackbar.make(viewHolder.itemView, "$removedKey removed", Snackbar.LENGTH_LONG).setAction("UNDO") {
+                list[removedKey] = removedValue
+                notifyItemInserted(removedPosition)
+                docRef.update(removedKey, removedValue)
+            }.show()
+        }
+
+
     }
 
     override fun getItemCount(): Int = list.size
--- app/src/main/java/com/tomas/vocabquiztest/MyVocabActivity.kt
+++ app/src/main/java/com/tomas/vocabquiztest/MyVocabActivity.kt
@@ -1,5 +1,9 @@
 package com.tomas.vocabquiztest
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,7 +11,12 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_my_vocab.*
 
@@ -15,8 +24,13 @@ import kotlinx.android.synthetic.main.activity_my_vocab.*
 class MyVocabActivity : AppCompatActivity() {
 
     private val db = FirebaseFirestore.getInstance()
-    private var vocabMap = Dictionary().vocabMap
+    private lateinit var docRef: DocumentReference
 
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    private lateinit var colorDrawableBackground: ColorDrawable
+    private lateinit var deleteIcon: Drawable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +38,8 @@ class MyVocabActivity : AppCompatActivity() {
 
         val playerId = intent.getStringExtra("playerId")
 
-        list_recycler_view.layoutManager = LinearLayoutManager(this)
+        colorDrawableBackground = ColorDrawable(Color.parseColor("#ff0000"))
+        deleteIcon = ContextCompat.getDrawable(this, R.drawable.ic_delete)!!
 
         db.collection(playerId)
             .get()
@@ -41,31 +56,77 @@ class MyVocabActivity : AppCompatActivity() {
             .addOnFailureListener { exception ->
                 Log.d("poop", "Error getting documents: ", exception)
             }
+
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, viewHolder2: RecyclerView.ViewHolder): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, swipeDirection: Int) {
+                (viewAdapter as ListAdapter).removeItem(viewHolder.adapterPosition, viewHolder, docRef)
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                val itemView = viewHolder.itemView
+                val iconMarginVertical = (viewHolder.itemView.height - deleteIcon.intrinsicHeight) / 2
+
+                if (dX > 0) {
+                    colorDrawableBackground.setBounds(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+                    deleteIcon.setBounds(itemView.left + iconMarginVertical, itemView.top + iconMarginVertical,
+                        itemView.left + iconMarginVertical + deleteIcon.intrinsicWidth, itemView.bottom - iconMarginVertical)
+                } else {
+                    colorDrawableBackground.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+                    deleteIcon.setBounds(itemView.right - iconMarginVertical - deleteIcon.intrinsicWidth, itemView.top + iconMarginVertical,
+                        itemView.right - iconMarginVertical, itemView.bottom - iconMarginVertical)
+                    deleteIcon.level = 0
+                }
+
+                colorDrawableBackground.draw(c)
+
+                c.save()
+
+                if (dX > 0)
+                    c.clipRect(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+                else
+                    c.clipRect(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+
+                deleteIcon.draw(c)
+
+                c.restore()
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
+        itemTouchHelper.attachToRecyclerView(list_recycler_view)
     }
 
     private fun populateSpinner(vocabNames: ArrayList<String>?, playerId: String) {
 
         val spinner = findViewById<Spinner>(R.id.spinner)
         if (spinner != null) {
-            val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, vocabNames!!.toArray())
+            val arrayAdapter =
+                ArrayAdapter(this, android.R.layout.simple_spinner_item, vocabNames!!.toArray())
             spinner.adapter = arrayAdapter
 
             spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                    val docRef = db.collection(playerId).document(vocabNames[position])
-                    docRef.get()
-                        .addOnSuccessListener { document ->
-                            if (document != null) {
-                                Log.d("poop", "DocumentSnapshot data: ${document.data}")
-                                vocabMap = document.data as Map<String, String>
-                                list_recycler_view.adapter = ListAdapter(vocabMap)
-                            } else {
-                                Log.d("poop", "No such document")
-                            }
-                        }
-                        .addOnFailureListener { exception ->
-                            Log.d("poop", "get failed with ", exception)
-                        }
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    docRef = db.collection(playerId).document(vocabNames[position])
+                    populateList()
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>) {
@@ -75,4 +136,28 @@ class MyVocabActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun populateList() {
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d("poop", "DocumentSnapshot data: ${document.data}")
+                    val vocabMap = document.data as MutableMap<String, String>
+                    viewAdapter = ListAdapter(vocabMap)
+                    viewManager = LinearLayoutManager(this)
+                    list_recycler_view.apply {
+                        setHasFixedSize(true)
+                        adapter = viewAdapter
+                        layoutManager = viewManager
+                        addItemDecoration(DividerItemDecoration(this.context, DividerItemDecoration.VERTICAL))
+                    }
+                } else {
+                    Log.d("poop", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("poop", "get failed with ", exception)
+            }
+    }
+
 }
